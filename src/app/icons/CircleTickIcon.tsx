1c5cd86704018251e7310adbf60ce55b801a19f7
paragmore <paragmore306@gmail.com>
2024-09-17 14:32:43 +0530

    icons

--- src/app/icons/CircleTickIcon.tsx
+++ src/app/icons/CircleTickIcon.tsx
@@ -10,11 +10,17 @@ export const CircleTickIcon = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect width="24" height="24" rx="12" fill="#F3F3F3" />
+        <rect
+          width="24"
+          height="24"
+          rx="12"
+          fillOpacity="0.10"
+          fill="currentColor"
+        />
         <path
           d="M10.8028 13.9025L16.318 8.38672L17.167 9.23512L10.8028 15.5993L6.98438 11.7809L7.83278 10.9325L10.8028 13.9025Z"
-          fill="#5F6065"
-          fill-opacity="0.25"
+          fill="currentColor"
+          fillOpacity="0.25"
         />
       </svg>
     </>
