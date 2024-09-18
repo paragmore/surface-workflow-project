48567bb224849da92a2466c15301895ef2553ca4
paragmore <paragmore306@gmail.com>
2024-09-18 18:26:49 +0530

    types

--- src/app/types/onboarding.ts
+++ src/app/types/onboarding.ts
@@ -1,8 +1,15 @@
 export enum ONBOARDING_STATUS {
-    INSTALL_TAG = 'installTag',
-    COPY_SNIPPET = 'copySnippet',
-    TEST_CONNECTION = 'testConnection',
-    INSTALL_SUCCESS = 'installSuccess',
-    INSTALL_FAIL = 'installFail',
-    TEST_TAG = 'testTag'
+  INSTALL_TAG = "installTag",
+  COPY_SNIPPET = "copySnippet",
+  TEST_CONNECTION = "testConnection",
+  INSTALL_SUCCESS = "installSuccess",
+  INSTALL_FAIL = "installFail",
+  TEST_TAG = "testTag",
+}
+
+export enum Status {
+  PENDING = "pending",
+  EXECUTING = "executing",
+  SUCCESSFUL = "successful",
+  FAILURE = "failure",
 }
