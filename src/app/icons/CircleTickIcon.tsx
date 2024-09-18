fcdcc45fe8e37952632db36c9fe3e2eda60b5005
paragmore <paragmore306@gmail.com>
2024-09-18 18:26:41 +0530

    status colors

--- src/app/icons/CircleTickIcon.tsx
+++ src/app/icons/CircleTickIcon.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-export const CircleTickIcon = () => {
+export const CircleTickIcon = ({
+  bgColor,
+  textColor,
+}: {
+  bgColor: string;
+  textColor: string;
+}) => {
   return (
     <>
       <svg
@@ -10,17 +16,11 @@ export const CircleTickIcon = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect
-          width="24"
-          height="24"
-          rx="12"
-          fillOpacity="0.10"
-          fill="currentColor"
-        />
+        <rect width="24" height="24" rx="12" fillOpacity="100" fill={bgColor} />
         <path
           d="M10.8028 13.9025L16.318 8.38672L17.167 9.23512L10.8028 15.5993L6.98438 11.7809L7.83278 10.9325L10.8028 13.9025Z"
-          fill="currentColor"
-          fillOpacity="0.25"
+          fill={textColor}
+          fillOpacity="0.40"
         />
       </svg>
     </>
