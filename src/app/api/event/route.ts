6b0f581311af500be2cf04287b2f43dd722b4e17
paragmore <paragmore306@gmail.com>
2024-09-18 19:40:34 +0530

    fix

--- src/app/api/event/route.ts
+++ src/app/api/event/route.ts
@@ -34,7 +34,7 @@ export async function POST(request: Request) {
     data: {
       name,
       visitor,
-      metadata,
+      metadata: metadata ?? {},
       createdAt: new Date(createdAt),
       tagId,
     },
