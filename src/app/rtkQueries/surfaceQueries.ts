9470878f41ceafbcdb8c838cf94180eb6ebaba65
paragmore <paragmore306@gmail.com>
2024-09-18 12:54:43 +0530

    rtk queries

--- src/app/rtkQueries/surfaceQueries.ts
+++ src/app/rtkQueries/surfaceQueries.ts
@@ -10,11 +10,32 @@ const baseQuery = (args, api, extraOptions) => {
 export const surfaceQueries = createApi({
   reducerPath: "surfaceQueries",
   baseQuery: baseQuery,
+  tagTypes: ["Tag", "Event"],
   endpoints: (builder) => ({
     getEvents: builder.query({
       query: () => "event",
     }),
+    getEventByTagId: builder.query({
+      query: ({ tagId }: { tagId: string }) => `event/${tagId}`,
+      providesTags: ["Event"],
+    }),
+    getTagByUser: builder.query({
+      query: ({ userName }: { userName: string }) => `tag/${userName}`,
+      providesTags: ["Tag"],
+    }),
+    addTagForUser: builder.mutation({
+      query: ({ userName }: { userName: string }) => {
+        const body = { userName };
+        return { url: "tag", method: "POST", body };
+      },
+      invalidatesTags: ["Tag"],
+    }),
   }),
 });
 
-export const { useGetEventsQuery } = surfaceQueries;
+export const {
+  useGetEventsQuery,
+  useGetTagByUserQuery,
+  useAddTagForUserMutation,
+  useGetEventByTagIdQuery,
+} = surfaceQueries;
