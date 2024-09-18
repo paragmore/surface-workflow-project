18e5b5b07cf6282a41b6c1bb5a0804327353006e
paragmore <paragmore306@gmail.com>
2024-09-18 18:26:34 +0530

    evnlt table

--- src/app/components/TestSurfaceTagsContent/EventTable/EventTable.tsx
+++ src/app/components/TestSurfaceTagsContent/EventTable/EventTable.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { EventTableRow } from "./EventTableRow";
 import { EventTableHeader } from "./EventTableHeader";
 import { type Event } from "@prisma/client";
+import { useGetEventsQuery } from "~/app/rtkQueries/surfaceQueries";
 
-export const EventTable = ({ eventData }: { eventData: Array<Event> }) => {
+export const EventTable = () => {
+  const { data: eventsResp, isLoading: isLoadingEvents } = useGetEventsQuery<{
+    data: { events: Array<Event> };
+    isLoading: boolean;
+  }>({});
   return (
     <table className="w-full rounded-lg border border-[#eaecf0] bg-white">
       <EventTableHeader />
       <tbody>
-        {eventData?.map((row, index) => (
+        {eventsResp?.events?.map((row, index) => (
           <EventTableRow
+            tagId={row?.tagId}
             id={row.id}
             key={index}
             name={row.name}
