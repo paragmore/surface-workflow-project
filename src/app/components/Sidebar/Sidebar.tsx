a12ed39dc467cb85f3f21fcffa88a1709c472187
paragmore <paragmore306@gmail.com>
2024-09-19 14:46:59 +0530

    hide sidebar

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -18,7 +18,7 @@ export const Sidebar = () => {
         {isCollapsed ? "→" : "←"}
       </button> */}
       <div
-        className={`sticky top-0 h-screen transition-all duration-300 xs:w-6 lg:w-64 bg-gray-800 text-white`}
+        className={`sticky top-0 h-screen transition-all duration-300 xs:w-0 lg:w-64 bg-gray-800 text-white`}
       >
         <div className="flex-start relative flex h-screen xs:w-[281px] lg:w-[281px] flex-col justify-between overflow-hidden rounded-br-[18px] rounded-tr-[18px] bg-[#f9f9f9] pb-10 shadow-[-3px_4px_10px_-3px_rgba(0,0,0,0.08)] shadow-[0px_1px_4px_0px_rgba(0,0,0,0.03)]">
           <SidebarBody />
